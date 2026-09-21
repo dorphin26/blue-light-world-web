e73c2d77d918e81d27bc7c063f21b130af63a5b5
dorphin26 <yeondorphin26@gmail.com>
2026-09-22 03:18:20 +0900

    feat: connect mobile joystick to player movement

--- src/scenes/ManorStudyScene.ts
+++ src/scenes/ManorStudyScene.ts
@@ -1,5 +1,5 @@
 import Phaser from "phaser";
-import { journalStore } from "../systems/journalStore";
+import { journalStore } from "../systems/journalStore";\nimport { mobileInputStore } from "../systems/mobileInputStore";
 
 type Interactable = {
   id: string;
